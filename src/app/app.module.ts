1290ccd3644f5a731eb92c7757dca3bdda351f9b
Federico Gallardo <fedegallar2006@hotmail.com>
2019-11-30 13:01:38 -0300

    Added a debuger component. Need implement it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import {NgxPaginationModule} from 'ngx-pagination'; // <-- import the module
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 import { MoviesPageComponent } from './movies-page/movies-page.component';
 import {MoviesListServiceService} from './movies-list-service.service';
-import { Movie } from './Movie';
 import { MovieViewComponent } from './movie-view/movie-view.component';
 import { routing } from './app.routes';
 import { MovieViewService } from './movie-view.service';
 import { TorrentplayerComponent } from './torrentplayer/torrentplayer.component';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +23,13 @@ import { TorrentplayerComponent } from './torrentplayer/torrentplayer.component'
   ],
   imports: [
     BrowserModule,
-    HttpClientModule, NgxPaginationModule,routing
+    HttpClientModule, 
+    NgxPaginationModule,
+    routing,
+    LoggerModule.forRoot({
+      serverLoggingUrl: '/api/logs', 
+      level: NgxLoggerLevel.DEBUG, 
+      serverLogLevel: NgxLoggerLevel.ERROR})
   ],
   providers: [MoviesListServiceService,MovieViewService],
   bootstrap: [AppComponent]
